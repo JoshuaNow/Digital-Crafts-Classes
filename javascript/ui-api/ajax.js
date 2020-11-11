fff83b515d108f2300be3f73b65ad3ca870e4295
Joshua Lopez <jrenelopez92@gmail.com>
2020-11-11 10:22:49 -0600

    more notes

---
+++ javascript/ui-api/ajax.js
@@ -0,0 +1,20 @@
+// Create an XMLHttpRequest object
+// Add an onreadystatechange event handler function
+// Set your handler function to check for a final readyState and a successful status
+// open() the request
+// send() the request
+
+// const request = new XMLHttpRequest();
+// request.onreadystatechange = function () {
+//   if (this.readyState === 4 && this.status === 200) {
+//     console.log(this);
+//   }
+// };
+// request.open("GET", "./sample.txt");
+// request.send();
+
+//this is a better way to do it by fetching api
+
+//fetch("./sample.txt").then((response) => {
+//   console.log(response);
+// });
